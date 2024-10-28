d42682c2dab09cad26722c73c95e8a5c0c1ed8a1
jai-gupta1 <freestylejai1@gmail.com>
2024-10-27 22:07:37 -0700

    ts to tsx change

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hi, I'm Jai!</p>
+        <a href='https://drive.google.com/file/d/1AdDnF-ojH6Ozk7BPaItwLFgM-0SDBqwq/view?usp=drive_link'>hi</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
